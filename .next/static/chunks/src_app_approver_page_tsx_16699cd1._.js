ab1a921b5faf6648bc6e71361b4d9be3bfc5b928
Alifta Maulana Rayhan <32029337+raayend@users.noreply.github.com>
2025-07-21 05:52:21 +0700

    Add .next/static/chunks/src_app_approver_page_tsx_16699cd1._.js 1753051940976

---
+++ .next/static/chunks/src_app_approver_page_tsx_16699cd1._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_008388a4._.js",
+  "static/chunks/node_modules_next_navigation_ff30cc2f.js"
+],
+    source: "dynamic"
+});
